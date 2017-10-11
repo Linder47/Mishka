8d53d305675729a45eea4054e58dc250a703f3f1
Linder47 <catty97@mail.ru>
2017-10-11 21:37:45 +0300

    almost done page 'index'

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function initMap() {
+    var coordinates = { lat: 59.936371, lng: 30.321663 },
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 17,
+      center: coordinates
+    }),
+  
+    markerImage = "img/map-pin.svg",
+    marker = new google.maps.Marker({
+        position: coordinates,
+        map: map,
+        icon: markerImage
+      });
+  }
