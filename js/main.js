5f65d084732eb781874c59a4b6fbb46c7f29d99e
Linder47 <catty97@mail.ru>
2017-10-15 02:57:08 +0300

    half done page 'catalog'

--- js/main.js
+++ js/main.js
@@ -12,3 +12,21 @@ function initMap() {
         icon: markerImage
       });
   }
+
+  var toggleClose = document.querySelector(".main-nav__toggle");
+  var mainNav = document.querySelector(".main-nav");
+  var toggleBurger = document.querySelector(".page-header__toggle");
+
+  toggleClose.addEventListener("click", function(event) {
+    event.preventDefault();
+    mainNav.classList.add("main-nav--close");
+  });
+
+  toggleBurger.addEventListener("click", function(event) {
+    event.preventDefault();
+    if (mainNav.classList.contains("main-nav--close")) {
+      mainNav.classList.remove("main-nav--close");
+    } else {
+      mainNav.classList.add("main-nav--close");
+    }
+  });
