aa51b9a921d462a2808ec9a0353636aa9415e36a
Linder47 <catty97@mail.ru>
2017-10-17 22:39:51 +0300

    done page 'catalog' + slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Map
+
 function initMap() {
     var coordinates = { lat: 59.936371, lng: 30.321663 },
     map = new google.maps.Map(document.getElementById('map'), {
@@ -12,6 +14,8 @@ function initMap() {
         icon: markerImage
       });
   }
+
+  //Navigation
 
   var toggleClose = document.querySelector(".main-nav__toggle");
   var mainNav = document.querySelector(".main-nav");
@@ -30,3 +34,46 @@ function initMap() {
       mainNav.classList.add("main-nav--close");
     }
   });
+
+  //Touch Slider
+
+  var reviewButtons = document.querySelectorAll(".about__toggle-item"),
+  reviewRadio = document.querySelectorAll('input[name="about__radio"]'),
+  reviewSlider = document.querySelector(".about__slider"),
+  swipeStartX = null,
+  swipeEndX = null,
+  page = 0;
+  
+  reviewSlider.addEventListener("touchstart", swipeStart);
+  reviewSlider.addEventListener("touchend", swipeEnd);
+  
+  
+  function swipeStart(event) {
+    setPage();
+    swipeStartX = event.changedTouches[0].screenX;
+  }
+  
+  function swipeEnd(event) {
+    setPage();
+    swipeEndX = event.changedTouches[0].screenX;
+    if (!swipeStartX || !swipeEndX) return;
+    if (swipeStartX < swipeEndX) {
+      if (reviewButtons[page - 1]) {
+        reviewButtons[page - 1].click();
+        page--;
+      }
+    } else if (swipeStartX > swipeEndX) {
+      if (reviewButtons[page + 1]) {
+        reviewButtons[page + 1].click();
+        page++;
+      }
+    }
+    swipeStartX = null;
+    swipeEndX = null;
+  }
+  
+  function setPage() {
+    reviewRadio.forEach( function(elem, number) {
+      if (elem.checked) page = number;
+    });
+  }
